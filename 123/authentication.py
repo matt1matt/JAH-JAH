79f39bebb9a19021ad242517d5fcf3a7d42e91a3
matt1matt <55518192+matt1matt@users.noreply.github.com>
2019-11-12 14:37:43 +0800

    Add files via upload

---
+++ 123/authentication.py
@@ -0,0 +1,19 @@
+import database as db
+
+def login(username, password):
+    is_valid_login = False
+    user=None
+    temp_user = db.get_user(username)
+    if(temp_user == None):
+        return "Invalid username or password. Please try again."
+    if(temp_user != None):
+        if(temp_user["password"]==password):
+            is_valid_login=True
+            user={"username":username,
+                  "first_name":temp_user["first_name"],
+                  "last_name":temp_user["last_name"]}
+        elif(temp_user["password"]==password):
+            return "Invalid username or password. Please try again."
+            
+    return is_valid_login, user
+
